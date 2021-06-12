6e957db4b26dfe48ad55455e8d08f47d4fe13aba
Mateusz Kot <mateuszkot@MacBook-Air-Mateusz.local>
2021-06-12 23:03:42 +0200

    Create navigation for landing-page

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { SiAffinitydesigner } from "react-icons/si";
+import "../styles/Navigation.css";
 
 const Navigation = () => {
-  return <nav></nav>;
+  return (
+    <nav className="navigation">
+      <div className="navigation-container">
+        <Link to="/" className="navigation-logo">
+          VOYAGE <SiAffinitydesigner className="logo" />
+        </Link>
+        <ul className="navigation-menu">
+          <li className="navigation-item">
+            <Link to="/" className="navigation-links">
+              Home
+            </Link>
+          </li>
+          <li className="navigation-item">
+            <Link to="/FaQ" className="navigation-links">
+              FaQ
+            </Link>
+          </li>
+          <li className="navigation-item">
+            <Link to="/contact" className="navigation-links">
+              Form
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
