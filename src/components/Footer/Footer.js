2681f717f62d217888e8a59646ab13a68a919da6
Mateusz Kot <mateuszkot@MacBook-Air-Mateusz.local>
2021-06-14 15:49:43 +0200

    Create Footer for landing-page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,53 @@
 import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { RiTwitterLine } from "react-icons/ri";
+import { FiFacebook } from "react-icons/fi";
+import { SiAffinitydesigner } from "react-icons/si";
+import { HiOutlineMailOpen } from "react-icons/hi";
+import { AiOutlinePhone, AiOutlineHome } from "react-icons/ai";
+import "../styles/Footer.css";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>Contact Info</h2>
+            <p>
+              <AiOutlineHome className="footer-link-items-home" /> Address:
+              st.XYZ 11, city Tychy
+            </p>
+            <p>
+              <AiOutlinePhone className="footer-link-items-phone" /> Phone:
+              222222222
+            </p>
+            <p>
+              <HiOutlineMailOpen className="footer-link-items-email" /> Email:
+              Vovage@xyz.com
+            </p>
+          </div>
+          <div className="footer-link-items">
+            <h2>Social Media</h2>
+            <p>
+              Facebook <FiFacebook className="footer-link-items-facebook" />
+            </p>
+            <p>
+              Instagram <FaInstagram className="footer-link-items-instagram" />
+            </p>
+            <p>
+              Twitter <RiTwitterLine className="footer-link-items-twitter" />
+            </p>
+          </div>
+        </div>
+      </div>
+      <section className="copyright">
+        <div className="copyright-rights">
+          © Voyage 2021 <SiAffinitydesigner className="copyright-logo" />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Footer;
